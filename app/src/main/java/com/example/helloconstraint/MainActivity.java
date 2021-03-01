6135bb21d24d06fb14d2ec9909a8f9f80d5a3920
sushant <shrestha.sushant22@gmail.com>
2021-03-01 10:01:08 +0545

    Homework 3rd commit

--- app/src/main/java/com/example/helloconstraint/MainActivity.java
+++ app/src/main/java/com/example/helloconstraint/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private Button btn_toast;
     private Button btn_count;
+    private Button btn_zero;
     private TextView counter;
     private int count=0;
     @Override
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btn_toast=findViewById(R.id.btntst);
         btn_count=findViewById(R.id.btncnt);
+        btn_zero=findViewById(R.id.btnzero);
         counter=findViewById(R.id.txtview);
         btn_toast.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +33,25 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 count=count+1;
                 counter.setText(""+count);
+                btn_zero.setBackgroundColor(btn_zero.getContext().getResources().getColor(R.color.my_light_blue, null));
+                if(count%2==0){
+                    //btn_count.setBackgroundColor(getResources().getColor(R.color.my_yellow, null));
+                    btn_count.setBackgroundColor(btn_count.getContext().getResources().getColor(R.color.my_green, null));
+                }
+                else {
+                    btn_count.setBackgroundColor(btn_count.getContext().getResources().getColor(R.color.my_red, null));
+                }
+
+            }
+
+        });
+        btn_zero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count=0;
+                counter.setText(""+count);
+                btn_zero.setBackgroundColor(btn_zero.getContext().getResources().getColor(R.color.my_grey, null));
+                btn_count.setBackgroundColor(btn_count.getContext().getResources().getColor(R.color.design_default_color_primary_variant, null));
             }
         });
     }
